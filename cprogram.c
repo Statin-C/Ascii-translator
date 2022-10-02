7fd203f2f45e1a73025b0d84b0c8d4c01d863bdd
Statin <81715696+Statin-C@users.noreply.github.com>
2022-10-01 22:26:57 -0300

    Add files via upload

---
+++ cprogram.c
@@ -0,0 +1,9 @@
+#include <stdio.h> /* printf and fprintf */
+#include <string.h>
+
+int main(){
+    int F = 0;
+    printf ("Write a number to translate it to Ascii Code!\n");
+    scanf ("%d", &F);
+      printf (&F); 
+}
